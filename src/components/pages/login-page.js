7ace8f2ffb3ad3130366fde02e8f917f64f05376
Touhid Mia <hmtm.cse@gmail.com>
2018-08-21 12:00:30 +0600

    Bismillah React and Material UI SPA

--- src/components/pages/login-page.js
+++ src/components/pages/login-page.js
@@ -1,16 +1,46 @@
-import { withStyles } from '@material-ui/core/styles';
 import React, { Component } from 'react';
+import { loginPageJSS } from './../../assets/jss/application-jss';
+import {
+    CssBaseline, Paper, Avatar, Typography, FormControl, InputLabel, Input, Button,
+    withStyles
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import LockIcon from '@material-ui/icons/LockOutlined';
 
-const styles = theme => ({
-    root: {
-        color:'blue'
-    }
-});
 
 class LoginPage extends Component {
     render () {
         const { classes } = this.props;
-        return <h1 className={classes.root}>LoginPage</h1>;
+        const loginUI = (
+            <React.Fragment>
+                <CssBaseline />
+                <main className={classes.layout}>
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <LockIcon />
+                        </Avatar>
+                        <Typography variant="headline">Login to Task Manager</Typography>
+                        <form method="get" action="/dashboard" className={classes.form}>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" autoComplete="email" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input  name="password" type="password" id="password"/>
+                            </FormControl>
+                            <Button type="submit" fullWidth variant="raised" color="primary" children="Sign in" className={classes.submit}/>
+                        </form>
+                    </Paper>
+                </main>
+            </React.Fragment>
+        );
+        return loginUI;
     }
 }
-export default withStyles(styles)(LoginPage);
+
+LoginPage.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(loginPageJSS)(LoginPage);
