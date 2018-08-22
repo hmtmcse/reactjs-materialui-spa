71d0e87c104b3533f7b4f42ffbee61b9718a6d13
Touhid Mia <hmtm.cse@gmail.com>
2018-08-22 21:46:07 +0600

    Bismillah React and Material UI SPA

--- src/components/app-component.js
+++ src/components/app-component.js
@@ -1,6 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+    Fade, LinearProgress, Checkbox
+} from '@material-ui/core';
 
 export default class AppComponent extends Component {
+
+    state = {
+        checkedA: true,
+        checkedB: true,
+        checkedF: true,
+    };
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.checked });
+    };
 
     appRender(){
         return (
@@ -8,5 +21,23 @@ export default class AppComponent extends Component {
         );
     }
 
-    render(){return this.appRender()}
+    render(){return(
+        <React.Fragment>
+
+            {
+                this.state.checkedA ? ( <Fade in={this.state.checkedA}>
+                    <LinearProgress color="primary" />
+                </Fade>) : ""
+            }
+
+
+            <Checkbox
+                checked={this.state.checkedA}
+                onChange={this.handleChange('checkedA')}
+                value="checkedA"
+            />
+
+            {this.appRender()}
+        </React.Fragment>
+    ) }
 }
