3e8fa79d899b2c1d53f266bfb4c0363ee017ff94
Touhid Mia <hmtm.cse@gmail.com>
2018-12-16 16:33:29 +0600

    Component Demo

--- src/components/experiment/url-map/views/user-create-update-view.js
+++ src/components/experiment/url-map/views/user-create-update-view.js
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 export default class UserCreateUpdateView extends Component {
     render() {
-        return (<h1>User Create Update View</h1>);
+        let id = this.props.route.match.params.id;
+        console.log(this.props);
+        console.log(id);
+        return (<h1>User Create Update View {id}</h1>);
     }
 }
