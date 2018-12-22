669019bb701bd4b00d477b014d7e906dfe2947be
Touhid Mia <hmtm.cse@gmail.com>
2018-12-22 21:40:46 +0600

    Component Demo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UrlMapping from "./components/experiment/url-map/url-mapping";
+import {LayoutRouter} from "./config/router";
+import FormValidationExample from "./components/experiment/form-validation/form-validation-example";
 
 
-ReactDOM.render(<UrlMapping/>, document.getElementById('root'));
+ReactDOM.render(<FormValidationExample/>, document.getElementById('root'));
+// ReactDOM.render(<LayoutRouter/>, document.getElementById('root'));
+// ReactDOM.render(<UrlMapping/>, document.getElementById('root'));
