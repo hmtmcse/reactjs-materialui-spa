deffcf987497d3795df0b01ab99fe00f3fa0a905
Touhid Mia <touhid@bitmascot.com>
2019-01-10 13:26:23 +0600

    drag and sort

---
+++ src/components/experiment/collapse-expanded/recipe-review-card-simple.js
@@ -0,0 +1,125 @@
+import React, {Component} from 'react';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+
+const styles = theme => ({
+    card: {
+        maxWidth: 400,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    actions: {
+        display: 'flex',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+    cardAction : {
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "8px",
+    },
+});
+
+
+class RecipeReviewCardSimple extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            expanded: false
+        };
+    }
+
+
+    handleExpandClick(event) {
+        event.preventDefault();
+        this.setState({expanded: !this.state.expanded})
+    }
+
+    render () {
+        const { classes } = this.props;
+        return (
+            <Card>
+                <CardActions className={classes.cardAction}>
+                    <div>
+                        <Typography variant="headline" align="center">Complexity</Typography>
+                    </div>
+                    <div>
+                        <IconButton><VisibilityIcon/></IconButton>
+                        <IconButton><EditIcon/></IconButton>
+                        <IconButton><DeleteIcon/></IconButton>
+                        <IconButton
+                            className={classnames(classes.expand, {
+                                [classes.expandOpen]: this.state.expanded,
+                            })}
+                            onClick={event => {
+                                this.handleExpandClick(event)
+                            }}
+                            aria-expanded={this.state.expanded}
+                            aria-label="Show more">
+                            <ExpandMoreIcon/>
+                        </IconButton>
+                    </div>
+                </CardActions>
+                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>Method:</Typography>
+                        <Typography paragraph>
+                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                            minutes.
+                        </Typography>
+                        <Typography paragraph>
+                            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                        </Typography>
+                        <Typography paragraph>
+                            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                            minutes more. (Discard any mussels that don’t open.)
+                        </Typography>
+                        <Typography>
+                            Set aside off of the heat to let rest for 10 minutes, and then serve.
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        );
+    }
+}
+export default withStyles(styles)(RecipeReviewCardSimple);
--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import FormValidationExample from "./components/experiment/form-validation/form-
 import DatePickerEx1 from "./components/experiment/datepicket/date-picker-ex1";
 import AtlassianDragAndDrop from "./components/experiment/drag-and-drop/atlassian-drag-and-drop";
 import AtlassianDragAndDropMui from "./components/experiment/drag-and-drop/atlassian-drag-and-drop-mui";
+import RecipeReviewCardSimple from "./components/experiment/collapse-expanded/recipe-review-card-simple";
 
 
-ReactDOM.render(<AtlassianDragAndDropMui/>, document.getElementById('root'));
+ReactDOM.render(<RecipeReviewCardSimple/>, document.getElementById('root'));
+// ReactDOM.render(<AtlassianDragAndDropMui/>, document.getElementById('root'));
 // ReactDOM.render(<DatePickerEx1/>, document.getElementById('root'));
 // ReactDOM.render(<FormValidationExample/>, document.getElementById('root'));
 // ReactDOM.render(<LayoutRouter/>, document.getElementById('root'));
